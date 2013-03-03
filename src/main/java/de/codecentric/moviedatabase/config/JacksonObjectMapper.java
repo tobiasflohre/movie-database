d51fb90bbfc37983ebc022cfc7a09cfda46b1fa4
Michael Lex <mich.lex@gmail.com>
2013-03-03 13:02:35 +0100

    Configure jackson-mapper, set FAIL_ON_UNKNOWN_PROPERTIES to false.

--- src/main/java/de/codecentric/moviedatabase/config/JacksonObjectMapper.java
+++ src/main/java/de/codecentric/moviedatabase/config/JacksonObjectMapper.java
@@ -4,12 +4,14 @@ import javax.annotation.PostConstruct;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.DeserializationConfig;
 
 public class JacksonObjectMapper extends ObjectMapper {
 	
 	
 	@PostConstruct
 	public void afterPropertiesSet() throws Exception {
+		configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS, false);
 	}
 	
