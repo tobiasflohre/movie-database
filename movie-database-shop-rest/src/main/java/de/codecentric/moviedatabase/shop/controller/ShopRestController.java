05ef258e9ad5bbb5fa7db3bc34a5cbcf1d91e623
tobiasflohre <tobias.flohre@codecentric.de>
2015-04-05 08:10:44 +0200

    Bugfixes in put

--- movie-database-shop-rest/src/main/java/de/codecentric/moviedatabase/shop/controller/ShopRestController.java
+++ movie-database-shop-rest/src/main/java/de/codecentric/moviedatabase/shop/controller/ShopRestController.java
@@ -1,13 +1,19 @@
 package de.codecentric.moviedatabase.shop.controller;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.codecentric.moviedatabase.shop.domain.Movie;
 import de.codecentric.moviedatabase.shop.repositories.MovieRepository;
@@ -18,13 +24,16 @@ public class ShopRestController {
 	
 	private MovieRepository movieRepository;
 	private String movieSystemBase;
+	private ObjectMapper objectMapper;
 	
 	@Autowired
 	public ShopRestController(MovieRepository movieRepository,
-			@Value("moviedatabase.movies.url.base") String movieSystemBase) {
+			@Value("moviedatabase.movies.url.base") String movieSystemBase,
+			ObjectMapper objectMapper) {
 		super();
 		this.movieRepository = movieRepository;
 		this.movieSystemBase = movieSystemBase;
+		this.objectMapper = objectMapper;
 	}
 
 	@RequestMapping
@@ -38,7 +47,8 @@ public class ShopRestController {
 	}
 
 	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
-	public void updateMovie(@PathVariable UUID id, Movie movie){
+	public void updateMovie(@PathVariable UUID id, @RequestBody String movieString) throws JsonParseException, JsonMappingException, IOException{
+		Movie movie = objectMapper.readValue(movieString, Movie.class);
 		Movie persistedMovie = movieRepository.findOne(id);
 		persistedMovie.setQuantity(movie.getQuantity());
 		persistedMovie.setPrice(movie.getPrice());
